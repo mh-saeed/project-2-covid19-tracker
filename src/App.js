b768ded878f756296019305eb8d437f937dd4028
mh-saeed <saeedkhan111244@gmail.com>
2021-08-04 00:35:44 +0500

    Project Structure Created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react'
-
-function App() {
-  return (
-    <div>
-     <h1>Hello React</h1>
-    </div>
-  );
+import React from "react";
+import { Cards, CountryPicker, Chart } from "./components";
+import styles from "./App.module.css";
+class App extends React.Component {
+  render() {
+    return (
+      <div className={styles.container}>
+        <h1>Hello React</h1>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
 }
 
 export default App;
