4904f0a39d0b48566237a7cc296142104ad5ae9a
mh-saeed <saeedkhan111244@gmail.com>
2021-08-05 01:29:34 +0500

    Line Chart completed

--- src/api/index.js
+++ src/api/index.js
@@ -9,9 +9,23 @@ export const fetchData = async () => {
     } = await axios.get(url);
 
     return { confirmed, recovered, deaths, lastUpdate };
-
   } catch (error) {
     return error;
   }
 };
 
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    const myData = data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      recovered: dailyData.recovered.total,
+      deaths: dailyData.deaths.total,
+      reportDate: dailyData.reportDate
+    }));
+    // console.log(myData)
+    return myData;
+  } catch (error) {
+    return error;
+  }
+};
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+import styles from "./Chart.module.css";
 
 const Chart = () => {
-    return (
-        <div>
-            <h1>Chart</h1>
-        </div>
-    )
-}
+  const [dailyData, setDailyData] = useState({});
 
-export default Chart
+  useEffect(() => {
+    const fetchMyAPI = async () => {
+      const initialDailyData = await fetchDailyData();
+
+      setDailyData(initialDailyData);
+    };
+    fetchMyAPI();
+  }, []);
+
+  console.log(dailyData);
+
+  const lineChart = dailyData[0] ? (
+    <Line
+      data={{
+        labels: dailyData.map((data) => new Date(data.reportDate).toLocaleDateString()),
+        datasets: [
+          {
+            data: dailyData.map((data) => data.confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {
+            data: dailyData.map((data) => data.deaths),
+            label: "Deaths",
+            borderColor: "red",
+            backgroundColor: "rgba(255, 0, 0, 0.5)",
+            fill: true,
+          },
+          {
+            data: dailyData.map((data) => data.recovered),
+            label: "Recovered",
+            borderColor: "green",
+            backgroundColor: "rgba(0, 255, 0, 0.5)",
+            fill: true,
+          },
+        ],
+      }}
+    />
+  ) : null;
+
+  return <div className={styles.container}>{lineChart}</div>;
+};
+
+export default Chart;
